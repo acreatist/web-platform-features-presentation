9cd098ac75578104dc0b3fc903314a32eba6fa1f
Dimitar Bonev <dsbonev@gmail.com>
2013-08-05 21:10:27 +0300

    add avatars to <template> demo

--- demos/template/js/main.js
+++ demos/template/js/main.js
@@ -1,15 +1,17 @@
 (function () {
   var users = [
-    {name: 'user_1', avatar: ''},
-    {name: 'user_2', avatar: ''}
+    {name: 'user_1', avatar: 'img/avatar_1.png'},
+    {name: 'user_2', avatar: 'img/avatar_2.jpg'}
   ];
 
   var userTemplate = document.querySelector('#user_template').content;
 
   users.forEach(function (user) {
     var userTemplateCopy = userTemplate.cloneNode(true);
-    userTemplateCopy.querySelector('img').src = user.avatar;
+
+    userTemplateCopy.querySelector('.avatar').src = user.avatar;
     userTemplateCopy.querySelector('.name').textContent = user.name;
+
     document.body.appendChild(userTemplateCopy);
   });
 })();
